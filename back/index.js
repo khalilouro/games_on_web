78c88816e8704c84d05cb01c8b47d28add16db43
khalil <rahmounimohamedkhalil@gmail.com>
2026-03-19 13:08:57 +0100

    fix

--- back/index.js
+++ back/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const app = express();
 const dotenv = require('dotenv');
 
 dotenv.config();
-const port =  process.env.PORT || 3000;
+
+const app = express();
 
 const connectDB = require('./connectDB/connectDB');
 connectDB();
@@ -18,11 +18,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../front/index.html'));
 });
 
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-
-    
+module.exports = app;
