62455f0ec389c45f76aee1cda38909f5e420587b
khalil <rahmounimohamedkhalil@gmail.com>
2026-03-19 12:57:07 +0100

    fix

--- back/connectDB/connectDB.js
+++ back/connectDB/connectDB.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        if (!process.env.DB_URL) {
+            console.log("❌ DB_URL est undefined");
+        }
+
         const conn = await mongoose.connect(process.env.DB_URL);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (err) {
-        console.error('MongoDB connection error:', err);
+        console.error("MongoDB connection error:", err.message);
     }
 };
 
