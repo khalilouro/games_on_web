6e2aa8311bfd3f75af531524b991b652d91d23c1
khalil <rahmounimohamedkhalil@gmail.com>
2026-03-19 14:47:18 +0100

    fix

--- back/index.js
+++ back/index.js
@@ -1,32 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-
-import connectDB from './connectDB/connectDB.js';
-
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const app = express();
-
-// équivalent de __dirname en ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const dotenv = require('dotenv');
 
 dotenv.config();
-const port = process.env.PORT || 3000;
+const port =  process.env.PORT || 3000;
 
+const connectDB = require('./connectDB/connectDB');
 connectDB();
 
 app.use(cors());
 app.use(express.json());
-
-// ⚠️ chemins
 app.use(express.static(path.join(__dirname, '../front')));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {    
     res.sendFile(path.join(__dirname, '../front/index.html'));
 });
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+    
